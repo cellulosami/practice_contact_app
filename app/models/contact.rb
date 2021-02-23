11ec80d0fd0de4221eb45a494835528b944228c7
austin shields <ajshields196@gmail.com>
2021-02-22 20:50:04 -0500

    remove written Contact model, add generated Contact model, and save several contacts in database.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,2 @@
-class Contact
-  def initialize(input_options)
-    @first_name = input_options[:first_name]
-    @last_name = input_options[:last_name]
-    @email = input_options[:email]
-    @phone_number = input_options[:phone_number]
-  end
+class Contact < ApplicationRecord
 end
