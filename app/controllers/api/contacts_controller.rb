411bf0ecce34c4d088886de1d990689e9c732140
austin shields <ajshields196@gmail.com>
2021-02-22 21:44:00 -0500

    add commented out version of all_contacts path

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -9,3 +9,14 @@ class Api::ContactsController < ApplicationController
     render "all_contacts.json.jb"
   end
 end
+
+# def display_all_contacts
+#   @contacts = Contact.all
+#   @information = ""
+#   @contacts.each do |contact|
+#     @information += "Contact id #{contact.id} is #{contact.first_name} #{contact.last_name}. Their email is #{contact.email}, and their phone number is #{contact.phone_number}.
+# "
+#   end
+#   render "all_contacts.json.jb"
+# end
+# end
